3aaa1698dd997e8ad56d46addb1e87ff14d837a5
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-04-13 23:06:41 -0300

    feat: inicialize header in Home Screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View, Image, StatusBar } from "react-native";
+import { ButtonAdd } from '../../components/ButtonAdd';
 import { Profile } from "../../components/Profile";
 import { styles } from "./styles";
 
@@ -8,6 +9,7 @@ export function Home() {
     <View style={styles.container}>
       <View style={styles.header}>
         <Profile />
+        <ButtonAdd />
       </View>
     </View>
   );
