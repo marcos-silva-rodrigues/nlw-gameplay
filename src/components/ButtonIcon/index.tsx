c589a46e3b7e424a953b364df48cbdfab9191d27
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-04-13 22:48:42 -0300

    refactor: change TouchableOpacity to RectButton

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,26 +1,21 @@
 import React from "react";
-import {
-  Image,
-  Text,
-  TouchableOpacity,
-  TouchableOpacityProps,
-  View,
-} from "react-native";
+import { Image, Text, View } from "react-native";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 import DiscordImg from "../../assets/discord.png";
 import { styles } from "./styles";
 
-type ButtonIconProps = TouchableOpacityProps & {
+type ButtonIconProps = RectButtonProps & {
   title: string;
 };
 
 export function ButtonIcon({ title, ...rest }: ButtonIconProps) {
   return (
-    <TouchableOpacity style={styles.container} {...rest}>
+    <RectButton style={styles.container} {...rest}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordImg} style={styles.icon} />
       </View>
 
       <Text style={styles.title}>{title}</Text>
-    </TouchableOpacity>
+    </RectButton>
   );
 }
