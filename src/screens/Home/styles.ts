173e0e223c0d9b09b942329b5d03d40515a642c1
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-04-15 20:04:24 -0300

    feat: use Appointment in FlatList and create ListDivider

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -14,5 +14,12 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: getStatusBarHeight() + 26,
     marginBottom: 42,
+  },
+  content: {
+    marginTop: 42
+  },
+  matches: {
+    marginTop: 24,
+    marginLeft: 24
   }
 });
