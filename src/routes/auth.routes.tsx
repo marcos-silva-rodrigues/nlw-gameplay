0a904c869feeebc32e840bea1735df7ba5cf54e5
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-04-16 18:51:08 -0300

    feat: AppoinmentScreen

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import { Home } from '../screens/Home';
-import { SignIn } from '../screens/SignIn';
-import { theme } from '../global/styles/theme';
+import { Home } from "../screens/Home";
+import { SignIn } from "../screens/SignIn";
+import { theme } from "../global/styles/theme";
+import { AppointmentDetails } from "../screens/AppointmentDetails";
 
 const Stack = createStackNavigator();
 
 export function AuthRoutes() {
-  return(
+  return (
     <Stack.Navigator
       screenOptions={{
         cardStyle: {
-          backgroundColor: theme.colors.secondary100
+          backgroundColor: theme.colors.secondary100,
         },
         headerShown: false,
-    }}
+      }}
     >
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="AppointmentDetail" component={AppointmentDetails} />
     </Stack.Navigator>
-  )
+  );
 }
---
+++ src/screens/AppointmentDetails/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Fontisto } from "@expo/vector-icons";
+import { BorderlessButton } from "react-native-gesture-handler";
+import { Background } from "../../components/Background";
+import { Header } from "../../components/Header";
+import { theme } from "../../global/styles/theme";
+
+export function AppointmentDetails() {
+  return (
+    <Background>
+      <Header
+        title="Detalhes"
+        action={
+          <BorderlessButton>
+            <Fontisto 
+              name="share"
+              size={24}
+              color={theme.colors.primary}
+            />
+          </BorderlessButton>
+        }
+      />
+    </Background>
+  );
+}
